b8b4ad39b15487e1028f505d509fa0154395a5fa
Filip Zagozdon <filipzagozdon8@gmail.com>
2022-08-30 22:42:56 +0200

    Refactor from constructor function to class syntax

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype = {
-    info: function() {
+    info() {
         const readString = this.read ? 'already read' : 'not read yet'
         return `${this.title} by ${this.author}, ${this.pages} pages, ${readString}`;
     }
